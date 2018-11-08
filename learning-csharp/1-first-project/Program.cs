428ab8b1a983c51801e772fac2f409e835cd9f78
Felipe Schossler <felipealvesschossler@gmail.com>
2018-11-08 16:34:33 -0200

    first commit with chapter 2 done

---
+++ learning-csharp/1-first-project/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_first_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("The execution is over. Press Enter to finalize the application...");
+            Console.ReadLine();
+        }
+    }
+}
