83fb3f6ea520fab85c0107267ca7ebad47b63c08
Felipe Schossler <felipealvesschossler@gmail.com>
2018-11-15 03:36:38 -0200

    Finished chapter 4

---
+++ learning-csharp/5-characters-and-texts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_characters_and_texts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Executando o projeto 5 - caracteres e texto");
+
+            char umaLetra = 'a';
+
+            Console.WriteLine(umaLetra);
+
+            string titulo = "Estou começando com isso e ainda vou fazer dar certo, etchola!!";
+
+            Console.WriteLine(titulo);
+
+            Console.WriteLine("Digite enter para finalizar o projeto.");
+            Console.ReadLine();
+        }
+    }
+}
